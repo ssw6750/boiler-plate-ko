2c94304972f4b2ce448edd9853402244753c843d
ex1 <skowko6750@naver.com>
2022-08-24 11:30:59 +0900

    비밀번호 암호화, 토큰 생성

--- models/User.js
+++ models/User.js
@@ -1,36 +1,86 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    maxlength: 50,
-  },
-  email: {
-    type: String,
-    trim: true,
-    unique: 1,
-  },
-  password: {
-    type: String,
-    minlength: 5,
-  },
-  lastname: {
-    type: String,
-    maxlength: 50,
-  },
-  role: {
-    type: Number,
-    default: 0
-  },
-  image: String,
-  token: {
-    type: String
-  },
-  tokenExp: {
-    type: Number
-  }
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
 });
+
+userSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+
+            if (err) 
+                return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) 
+                    return next(err)
+                user.password = hash
+                next()
+            });
+        });
+    } else {
+      next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+
+  // plainPassword 1234567  암호화된 비밀번호 "$2b$10$mVjEZzkg3nTTUdaxQHaw8eSSyK1AB5PX3Lvp3xES5AW7XHaeY4BOC"
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if(err) return cb(err)
+    cb(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function(cb) {
+
+  var user = this;
+
+  // jsonwebtoken을 이용해서 토큰을 생성하기
+  var token = jwt.sign(user._id.toHexString(), "secretToken")
+
+  user.token = token
+  user.save(function(err, user) {
+    if(err) return cb(err)
+    cb(null, user)
+  })
+}
 
 const User = mongoose.model('User', userSchema)
 
-module.exports = {User}
+module.exports = {
+    User
+}

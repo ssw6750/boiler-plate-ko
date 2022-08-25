f8489e2e138e61ae2f83802cfc3d439b8fa1f1fa
ex1 <skowko6750@naver.com>
2022-08-25 16:46:19 +0900

    클라이언트 폴더 정리

--- server/index.js
+++ server/index.js
@@ -25,6 +25,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/api/hello", (req, res)=> {
+  res.send("안녕하세요")
+})
+
 
 app.post('/api/users/register', (req, res) =>{
   

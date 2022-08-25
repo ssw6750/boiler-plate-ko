5ee104ded7096b55300cdcd93082f0e75864273d
ex1 <skowko6750@naver.com>
2022-08-25 16:58:14 +0900

    CORS 이슈, Proxy 설정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ function LandingPage() {
 
     useEffect(() => {
         axios
-            .get("http://localhost:5000/api/hello")
+            .get("/api/hello")
             .then((response) => console.log(response.data));
     }, [])
 
